f705dc3f76ddb8f9837fdbca260817457159d34e
awzuelsdorf <azuelsdorf16@gmail.com>
2015-10-29 19:51:58 -0500

    Now enterCodes.sh will append data to log and error files instead of erasing their contents.

--- enterCodes.sh
+++ enterCodes.sh
@@ -4,6 +4,5 @@ if [ $# != 3 ]; then
     echo "Usage: $0 <codes_caps_file> <codes_cardboard_file> <credentials_file>" 1>&2;
     exit 1;
 else
-    echo "Congratulations, you won!"
-    ./mycoke2.py $1 $2 < $3 1> output 2> errput
+    ./mycoke2.py $1 $2 < $3 1>> output 2>> errput
 fi
